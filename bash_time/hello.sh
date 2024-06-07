34f2faf9e51d9a90ea819fa5e3b8867910fe67bd
Sourabh Jaiswal <sourabhrj31@gmail.com>
2024-06-07 17:40:13 +0530

    stdin

--- bash_time/hello.sh
+++ bash_time/hello.sh
@@ -1,3 +1,19 @@
 #! /bin/bash
 
 echo "Hello, World!"
+count=10
+
+# if [ $count -eq "$1" ]; then
+# 	echo "Count is $1"
+# else
+# 	echo "Count is not 10 its $1"
+# fi
+#
+while [ $count -lt 20 ]; do
+	echo "Count is $count"
+	((count++))
+done
+
+while read -r line; do
+	echo "$line"
+done <"${1:-/dev/stdin}"
